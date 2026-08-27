a2b96eb670b705bf3f22130be2b3e2605d7865da
pir9x6 <pierre.blache.54@gmail.com>
2026-08-27 20:34:28 +0200

    updated libs

---
+++ tips_kicad.sh
@@ -0,0 +1,23 @@
+# Hotkeys
+W Wires
+N Place Nets
+B Bus
+E Propriétés
+F1 Switch to BRD/SCH
+F2 Zoom to fit
+
+# Headers value
+HDR_2X6_p200
+HDR_2X16_p254_RA
+
+# Symboles
+taille text = 1.524 mm (60 mils)
+
+# Editeur de symboles (bug)
+parfois la liste des symboles n apparait pas
+View -> Panels -> Library tree
+
+# Silkscreen
+Largeur x Hauteur : 1.5 x 1.5 mm
+Épaisseur : 0.2 mm
+Point pin 1 : Rayon = 0.2 mm, épaisseur = 0.4 mm
